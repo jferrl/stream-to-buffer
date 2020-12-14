ce974c5ae732b48ebfcdf22189779457b9963f96
Jorge Ferreero Linacero <jorge.ferreero.linacero@hp.com>
2020-12-14 13:15:33 +0100

    Throw error when stream is not defined

--- lib/stream-to-buffer.ts
+++ lib/stream-to-buffer.ts
@@ -1,4 +1,5 @@
 import { BufferableStream } from './buffer-stream';
+import { InvalidStreamError } from './error';
 
 /**
  * @Method: Returns the content of the readable stream as a buffer
@@ -6,6 +7,10 @@ import { BufferableStream } from './buffer-stream';
  * @Return {Promise<Buffer>}
  */
 export async function streamToBuffer(stream: NodeJS.ReadableStream): Promise<Buffer> {
+    if (!stream) {
+        throw new InvalidStreamError('stream is not defined');
+    }
+
     const bufferableStream = new BufferableStream();
 
     return new Promise((resolve: (data: Buffer) => void, reject: (error: Error) => void): void => {
